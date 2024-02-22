a297e4a34da1055d15c43d00de6873ab3422fe23
jmh8fr <jmh8fr@virginia.edu>
2024-02-22 13:31:45 -0500

    Initial commit of madlib.sh script

---
+++ madlib.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Please give me an adjective: " ADJ1
+read -p "2. Please give me a verb: " VRB1
+read -p "3. Please give me a noun: " NOU1
+read -p "4. Please give me an adverb: " ADV1
+read -p "5. Please give me an adjective: " ADJ2
+read -p "6. Pleaee give me a verb: " VRB2
+read -p "7. Please give me a noun: " NOU2
+read -p "8. Please give me an adverb: " ADV2
+
+echo "Once upon a time, in a faraway land, there was a $ADJ1 $NOU1. This $NOU1 was known throughout the kindgom for its $ADJ2 nature."
+echo "One day, the $NOU1 stumbled upon another $NOU2 that was in trouble. Without hesitation, it $VRB1 to help."
+echo "The $NOU2 was extremely grateful, saying, 'Thank You! Not only are you $ADJ1 and $ADJ2, but you acted so $ADV1!'"
+echo "From that day forward, the $NOU1 and the $NOU2 became the best of friends, often seen $VRB2 around the knigdom, spreading joy wherever they went."
+echo "And so, the tale of the $ADJ1 $NOU1 and its $NOU2 friend, so $ADV2 told, became a legend."
